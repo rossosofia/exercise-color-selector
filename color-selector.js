19b551d349eacb71ebb1187f25ba0e5d38e888d7
sofia amoroso <sofi79a0@stud.kea.dk>
2023-02-15 18:35:17 +0100

    display ok, hsl to improve

--- color-selector.js
+++ color-selector.js
@@ -5,22 +5,43 @@ window.addEventListener("DOMContentLoaded" , start);
 function start(){
     console.log("START");
     addEventListeners();
-}
+    colorUpdate(input.value);
+  }
 
 function addEventListeners(){
-    document.querySelector("input").addEventListener("input" , colorChange);
-
+  document.querySelector("input").addEventListener("input" , colorConvert);
 }
 
-function colorChange(){
+function colorConvert(){
     // from mdn web docs: The value of an <input> element of type color is always a string which contains a 7-character string specifying an RGB color in hexadecimal format.
-    console.log("HEX: ", this.value);
+    // console.log("HEX: ", this.value);
     // use variables to store the rgb and hsl. Start from the HEX value to get RGB value, calling the function to convert
     let rgb = hexToRGB(this.value);
     // after you get RBG value, convert it in HSL
     let hsl = RGBtoHSL(rgb); //let hsl = RGBtoHSL(hexToRGB(this.value));
-    console.log(` RGB: , ${rgb.r}, ${rgb.g} , ${rgb.b}`);
-    console.log(`HSL: , ${hsl.h} , ${hsl.s}% , ${hsl.l}%`);
+    // console.log(` RGB: , ${rgb.r}, ${rgb.g} , ${rgb.b}`);
+    // console.log(`HSL: , ${hsl.h} , ${hsl.s}% , ${hsl.l}%`);
+    //change element colours
+    colorUpdate(this.value, this.value, rgb, hsl)
+}
+
+function colorUpdate(css, hex, rgb, hsl){
+  // from exercise RandomBackgroundColor
+  document.querySelector("div").style.backgroundColor = css;
+  document.getElementById("hex").textContent = hex;
+  document.getElementById("rgb").textContent = `RGB  | ${rgb.r} - ${rgb.g} - ${rgb.b}`;
+  document.getElementById("hsl").textContent = `HSL | ${hsl.h} - ${hsl.s}% - ${hsl.l}%`;
+}
+
+
+
+function hexToRGB(hexColor){
+  // The parseInt() function parses a string argument and returns an integer of the specified radix. parseInt(string, radix). With substring we are collecting the 2 numbers at time from the HEX string and using them for the calculation
+  const r = parseInt(hexColor.substring(1,3),16);
+  const g = parseInt(hexColor.substring(3,5),16);
+  const b = parseInt(hexColor.substring(5,7),16);
+  //return (${r},${g},${b});
+  return {r,g,b};
 }
 
 // function given in the assignment
@@ -66,11 +87,3 @@ l *= 100;
 return {h: Math.floor(h) ,s: Math.floor(s),l: Math.floor(l)}
 }
 
-function hexToRGB(hexColor){
-    // The parseInt() function parses a string argument and returns an integer of the specified radix. parseInt(string, radix). With substring we are collecting the 2 numbers at time from the HEX string and using them for the calculation
-    const r = parseInt(hexColor.substring(1,3),16);
-    const g = parseInt(hexColor.substring(3,5),16);
-    const b = parseInt(hexColor.substring(5,7),16);
-    //return (${r},${g},${b});
-    return {r,g,b};
-}
